a399f06a2b12cdb78859f48c8a52e8a86318f2f0
Uday vara <varaudayd@gmail.com>
2024-05-21 22:57:03 +0530

    --->module import error fixes

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,13 +4,12 @@ import { AppService } from './app.service';
 import { DocumentModule } from './document/document.module';
 import { UserModule } from './user/user.module';
 import { JwtModule } from '@nestjs/jwt';
-import { StreamModule } from './stream/stream.module';
 
 @Module({
   imports: [JwtModule.register({
     secret:"thisismyrandomjwtsecret@1223334444",
     global:true,
-  }),UserModule, DocumentModule, StreamModule],
+  }),UserModule, DocumentModule],
   controllers: [AppController],
   providers: [AppService],
 })
